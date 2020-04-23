bc424058275da52731db00bbc6a5b351c60680dc
EC2 Default User <ec2-user@ip-172-31-60-3.ec2.internal>
2020-04-23 21:40:05 +0000

    test

--- __tests__/list.test.js
+++ __tests__/list.test.js
@@ -1,10 +1,12 @@
-import axios from 'axios';
-import { ServiceEndpoint as API_GATEWAY_BASE_URL } from '../.build/stack.json';
+const axios = require('axios');
+const stack = require('../.build/stack.json');
+
+console.log("stack.ServiceEndpoint: " + stack.ServiceEndpoint);
 
 const instance = axios.create({
-    baseURL: `${API_GATEWAY_BASE_URL}`,
+    baseURL: stack.ServiceEndpoint,
     timeout: 1000,
-    headers: {'requestContext': '{cognitoIdentityId: "USER-SUB-1234"}'}
+    headers: {'requestContext': '{cognitoIdentityId: "b722f9fe-3639-473e-9595-6986161019c2"}'}
 });
 
 test('list all notes for user', async () => {
